51c000a562fda2b4b97a26ef13e93db2544a0665
Sergio Santiago <sersanhen@gmail.com>
2018-06-23 03:38:47 +0200

    ADD: entidad User y prueba de listado de usuarios

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->findAll();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController'
+            'users' => $users
         ]);
     }
 }
