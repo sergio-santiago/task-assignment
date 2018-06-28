84e59632ce6c94098a2350b7fd8827d0d27fd675
Sergio Santiago <sersanhen@gmail.com>
2018-06-28 17:02:55 +0200

    ADD: bootbox y mejorada confirmacion eliminar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,30 @@
 const $ = require('jquery');
 require('bootstrap-sass');
+let bootbox = require('bootbox');
 
-$(document).ready(function () {
-    $('[data-toggle="popover"]').popover();
+/**
+ * CONFIRMATION MENU
+ * Boton dentro de formulario con =>
+ * clase -> launch-confimation-js
+ * atributos -> data-message-text data-confirm-text data-cancel-text
+ */
+$('.launch-confimation-js').click(function () {
+    bootbox.confirm({
+        message: $(this).data().messageText,
+        buttons: {
+            confirm: {
+                label: $(this).data().confirmText,
+                className: 'btn-success'
+            },
+            cancel: {
+                label: $(this).data().cancelText,
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            if (result === true) {
+                $('.launch-confimation-js').closest('form').submit();
+            }
+        }
+    });
 });
