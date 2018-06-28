dc59899639f38671166d28690bff9f74a7cb3ca9
Sergio Santiago <sersanhen@gmail.com>
2018-06-28 21:29:03 +0200

    ADD: juntado js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,29 +2,75 @@ const $ = require('jquery');
 require('bootstrap-sass');
 let bootbox = require('bootbox');
 
-/**
- * CONFIRMATION MENU
- * Boton dentro de formulario con =>
- * clase -> launch-confimation-js
- * atributos -> data-message-text data-confirm-text data-confirm-button-style data-cancel-text data-cancel-button-style
- */
-$('.launch-confimation-js').click(function () {
-    bootbox.confirm({
-        message: $(this).data().messageText,
-        buttons: {
-            confirm: {
-                label: $(this).data().confirmText,
-                className: 'btn-' + $(this).data().confirmButtonStyle
-            },
-            cancel: {
-                label: $(this).data().cancelText,
-                className: 'btn-' + $(this).data().cancelButtonStyle
-            }
-        },
-        callback: function (result) {
-            if (result === true) {
-                $('.launch-confimation-js').closest('form').submit();
-            }
+let app = {};
+app = function () {
+    let events = function () {
+        $('.select-role-input-js').change(function () {
+            isActiveCheckboxPermission();
+        });
+
+        $('.delete-launch-confimation-info-js').click(function () {
+            deleteLaunchConfimationInfo();
+        });
+    };
+
+    /**
+     * Bloqueo checkbox en edit y add user
+     */
+    function isActiveCheckboxPermission() {
+        let option = $('.select-role-input-js').val();
+        let checkboxInput = $('.is-active-checkbox-input-js');
+        let checkboxLabel = $('.is-active-checkbox-label-js');
+        let checkboxHelp = $('.is-active-checkbox-help-js');
+        if (option === 'ROLE_ADMIN') {
+            checkboxInput.prop('disabled', true);
+            checkboxInput.prop('checked', true);
+            checkboxLabel.css('color', '#6c747d');
+            checkboxLabel.css('cursor', 'not-allowed');
+            checkboxHelp.show();
+        } else {
+            checkboxInput.prop('disabled', false);
+            checkboxLabel.css('color', '#333333');
+            checkboxLabel.css('cursor', 'pointer');
+            checkboxHelp.hide();
         }
-    });
+    }
+
+    /**
+     * Confirmacion eliminacion usuario en vista info
+     */
+    function deleteLaunchConfimationInfo() {
+        let button = $('.delete-launch-confimation-info-js');
+        bootbox.confirm({
+            message: button.data('messageText'),
+            buttons: {
+                confirm: {
+                    label: button.data('confirmText'),
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: button.data('cancelText'),
+                    className: 'btn-default'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    button.closest('form').submit();//SI SE PONE ID A FORMULARIO CAMBIAR...
+                }
+            }
+        });
+    }
+
+    let init = function () {
+        isActiveCheckboxPermission();
+        events();
+    };
+
+    return {
+        init: init
+    }
+}();
+
+$(function () {
+    app.init();
 });
