590b434865f9370268e6b0f953db1cb6c6aa2843
Sergio Santiago <sersanhen@gmail.com>
2018-07-23 22:19:43 +0200

    ADD: controller home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class HomeController extends Controller
+{
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('home/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    public function homepage()
+    {
+        return $this->render('home/homepage.html.twig');
+    }
+}
