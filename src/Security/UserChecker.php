f72ac7e4e05a903829baf3b3cd6df2f02738ca45
Sergio Santiago <sersanhen@gmail.com>
2018-07-24 20:29:51 +0200

    ADD: validacion de usuario activo en login

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+        if (!$user->getIsActive()) {
+            throw new AccountInactiveException();
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+    }
+}
